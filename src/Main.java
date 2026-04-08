27baa0a377ef2c97dcfd25bc925fa770998695d8
Gulzat <aldashovagulzat02@gmail.com>
2026-04-08 11:07:27 +0600

    Commit test to main

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+
+        System.out.println("Java 17");
     }
 
     public void add(){
